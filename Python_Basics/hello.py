387b5a51d7e8b9e8dad5ea8aa1cea0d473b9bad0
Flyhiee <flyhieeofficial@gmail.com>
2019-12-01 19:00:31 +0530

    Added first lession

---
+++ Python_Basics/hello.py
@@ -0,0 +1,25 @@
+#print Hello Amit
+print('Hello Amit!')
+
+"""
+A Syntactic error is when Python does not understand your code. For example, if you spell print "frint", you will get an error
+message.
+
+
+A Semantic error is when your logic is wrong.
+For example, if you enter Python 102 instead of Python 101, you don't get an error message,
+but your code is wrong.
+
+
+We can see the actual data type in Python by using the type command.
+
+
+You can change the type of the expression in Python; this is called type-casting. You can convert an int to a float.
+For example, you can convert or cast the integer 2 to a float 2.
+
+
+We call the numbers operands and the maths symbols, in this case addition, are called operators.
+
+
+We can use the double slash for integer division, where the result is rounded.
+"""
